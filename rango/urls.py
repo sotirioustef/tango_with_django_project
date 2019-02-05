066532266642add5ff42922f7b0d6466cf9054db
Stefanos Sotiriou <2410767s@student.gla.ac.uk>
2019-02-05 14:18:51 +0000

    chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	#url(r'^$', views.about, name='about'),
 	url(r'^about/', include('about.urls')),
+	url(r'^add_category/$', views.add_category, name='add_category'),
 	#url(r'^about/$', views.about, name='about'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
 		views.show_category, name='show_category'),
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+
 ]
