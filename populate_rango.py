b342cd2b1d76ee5a8267dd34c573c87bab5a5484
Stefanos Sotiriou <2410767s@student.gla.ac.uk>
2019-02-11 12:40:13 +0000

    New changes

--- populate_rango.py
+++ populate_rango.py
@@ -42,7 +42,7 @@ def populate():
 
     cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
         "Django": {"pages": django_pages, "views":64, "likes":32},
-        "Other Frameworks": {"pages": other_pages, "views":0, "likes":0}}
+        "Other Frameworks": {"pages": other_pages, "views":32, "likes":16}}
 
     for cat, cat_data in cats.items():
         c= add_cat(cat, cat_data["views"], cat_data["likes"])
